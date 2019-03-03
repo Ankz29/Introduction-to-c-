be3b159f844e60bc58d3f0574ee2b69302742874
Ankita <ankita.m@softwaysolutions.com>
2019-03-03 11:28:15 +0530

    this is a poc on c# introduction

---
+++ IntroductionToCsharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace IntroductionToCsharp
+{
+    class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Welcome to c#.");
+            
+
+        }
+
+        public static void Main2()
+        {
+            Console.WriteLine("Training");
+            
+        }
+
+        public static void Main3()
+        {
+            Console.WriteLine("tuorials");
+            
+        }
+
+        public static void Main(string [] args)
+        {
+            Main1();
+            Main2();
+            Main3();
+            Console.ReadLine();
+        }
+        
+    }
+}
